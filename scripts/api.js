b301f086e11faa29690a70658e6e9fe87612d1ea
Burkey Koontz <bk.koontz@gmail.com>
2018-07-24 12:11:43 -0700

    finished api.js

--- scripts/api.js
+++ scripts/api.js
@@ -1,4 +1,25 @@
+// Declare a BASE_URL constant appropriate for the Youtube API.
+// Expose an asyncronous fetchVideos function, which receives a searchTerm and callback
+// The function will retrieve Youtube data, filter out only the content it needs 
+// (video title, image and link), and send that array back in the callback
+
 'use strict';
 /* global $ */
 
+const api = (function(){
+  const BASE_URL = 'https://www.googleapis.com/youtube/v3/search';
 
+  const fetchVideos = function(searchTerm, callback) {
+    const query = {
+      key: API_KEY,
+      q: `${searchTerm} in:name`,
+      per_page: 5,
+      part: 'snippet'
+    };
+    $.getJSON(BASE_URL, query, callback);
+  };
+
+  return {
+    fetchVideos
+  };
+}());
